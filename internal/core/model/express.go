72d24a915b72319a38c9b06f58678ef9e23f88de
yangshujie <yangshujie@fangcunyisheng.com>
2023-07-26 17:41:34 +0800

    feat 实现 express struct

--- internal/core/model/express.go
+++ internal/core/model/express.go
@@ -1,9 +1,37 @@
 package model
 
-type ExpressSender struct{}
+// ExpressSender 快递寄件人
+type ExpressSender struct {
+	Name   string
+	Mobile string
+}
 
-type ExpressReceiver struct{}
+// ExpressReceiver 快递收件人
+type ExpressReceiver struct {
+	Name   string
+	Mobile string
+}
 
-type ExpressDelivery struct{}
+// ExpressPackage 快递包裹
+type ExpressPackage struct {
+	Length         float64
+	Width          float64
+	Height         float64
+	Weight         float64
+	ExpressNo      string
+	ExpressCompany ExpressCompany
+}
 
-type ExpressAddress struct{}
+// ExpressAddress 快递地址
+type ExpressAddress struct {
+	Province string
+	City     string
+	County   string
+	Address  string
+}
+
+// ExpressCompany 快递公司
+type ExpressCompany struct {
+	Name string
+	Code string
+}
